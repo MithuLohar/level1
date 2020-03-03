28de48b3285d731b685f20912e9c5599e90b258e
MithuLohar <mithulohar9@gmail.com>
2020-03-03 10:09:08 +0530

    level3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { FormsModule, NgForm } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'elmeasure';
+  title = "elmeasure";
+  firstnumber;
+  secondnumber;
+  Result;
+
+  submit(form: NgForm) {
+    this.firstnumber = form.value.input1;
+    this.secondnumber = form.value.input2;
+
+    const first = +this.firstnumber;
+    const second = +this.secondnumber;
+    const result = first * second;
+    console.log(result);
+    this.Result = result;
+  }
 }
